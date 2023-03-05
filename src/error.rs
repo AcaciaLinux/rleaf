28c11b80b508617d41557b88354b5ff56514356a
Max Kofler <kofler.max.dev@gmail.com>
2023-03-05 18:40:09 +0100

    error: LeafError -> LeafCoreError, LeafConfigError
    LeafError has now become LeafCoreError, it describes error in the leaf
    core
    LeafConfigError describes config errors

--- src/error.rs
+++ src/error.rs
@@ -1,15 +1,32 @@
 use crate::rleaf_sys::{cleafcore, cleafcore_getError, cleafcore_getErrorString};
 use std::{error::Error, fmt};
 
+#[allow(non_camel_case_types)]
 #[derive(Debug)]
-pub struct LeafError{
+pub enum LeafConfigErrorClass {
+    OK = 0,
+    UNKNOWN = 1,
+    NOTINIT = 255,
+    NOCORE = 254,
+    RO_CONF = 253,
+    INV_CONF = 252,
+}
+
+#[derive(Debug)]
+pub struct LeafCoreError {
     code: u16,
     message: String,
 }
-impl LeafError{
+
+#[derive(Debug)]
+pub struct LeafConfigError {
+    class: LeafConfigErrorClass,
+}
+
+impl LeafCoreError {
     /// Creates a new LeafError instance from the last error set in the cleaf API
-    pub fn new_from_last(core: &cleafcore) -> LeafError{
-        let e = LeafError { 
+    pub fn new_from_last(core: &cleafcore) -> LeafCoreError {
+        let e = LeafCoreError {
             code: unsafe { cleafcore_getError(core) },
             message: crate::util::from_c_str(unsafe { cleafcore_getErrorString(core) })
         };
@@ -17,8 +34,8 @@ impl LeafError{
         e
     }
 }
-impl Error for LeafError{}
-impl fmt::Display for LeafError{
+impl Error for LeafCoreError {}
+impl fmt::Display for LeafCoreError {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         write!(f, "Leaf error: '{}', code: {}", self.message, self.code)
     }
