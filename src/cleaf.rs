17bad7ce3e21b1eb82fcf9f559eb1f481f0c35f0
Max Kofler <kofler.max.dev@gmail.com>
2023-03-05 18:53:00 +0100

    cleaf: cargo fmt

--- src/cleaf.rs
+++ src/cleaf.rs
@@ -1,24 +1,30 @@
-use crate::rleaf_sys::{cleaf_init, cleaf_finalize};
+use crate::rleaf_sys::{cleaf_finalize, cleaf_init};
 use std::sync::atomic::{AtomicU32, Ordering};
 static LEAF_INSTANCES: AtomicU32 = AtomicU32::new(0);
 
 /// Registers a new Leafcore instance. If no cleaf context exists, this creates one
-pub fn add_leaf_instance(){
-    if LEAF_INSTANCES.load(Ordering::Relaxed) == 0{
+pub fn add_leaf_instance() {
+    if LEAF_INSTANCES.load(Ordering::Relaxed) == 0 {
         unsafe { cleaf_init(3) };
         debug!("[rleaf] Initializing cleaf");
     }
 
     LEAF_INSTANCES.fetch_add(1, Ordering::Relaxed);
-    trace!("[rleaf] Registering new leaf instance, {} in total now", LEAF_INSTANCES.load(Ordering::Relaxed));
+    trace!(
+        "[rleaf] Registering new leaf instance, {} in total now",
+        LEAF_INSTANCES.load(Ordering::Relaxed)
+    );
 }
 
 /// Unregister a Leafcore instance. If no instances are left, this cleans up the cleaf context
-pub fn remove_leaf_instance(){
+pub fn remove_leaf_instance() {
     LEAF_INSTANCES.fetch_sub(1, Ordering::Relaxed);
-    trace!("[rleaf] Unregistering a leaf instance, {} remaining", LEAF_INSTANCES.load(Ordering::Relaxed));
+    trace!(
+        "[rleaf] Unregistering a leaf instance, {} remaining",
+        LEAF_INSTANCES.load(Ordering::Relaxed)
+    );
 
-    if LEAF_INSTANCES.load(Ordering::Relaxed) == 0{
+    if LEAF_INSTANCES.load(Ordering::Relaxed) == 0 {
         unsafe { cleaf_finalize() };
         debug!("[rleaf] Finalizing cleaf");
     }
